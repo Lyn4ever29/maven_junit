13d7c7df1640508b1b2bd4f47bc4091acb7d9206
lyn4ever29 <Lyn4ever29@163.com>
2020-04-24 00:19:52 +0800

    complete

--- spring-junit5/src/test/java/cn/lyn4ever/test/TestDemo.java
+++ spring-junit5/src/test/java/cn/lyn4ever/test/TestDemo.java
@@ -1,6 +1,10 @@
 package cn.lyn4ever.test;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
 
 /**
  * 微信公众号 “小鱼与Java”
@@ -11,7 +15,12 @@ import org.junit.jupiter.api.Test;
 public class TestDemo {
 
     @Test
-    void fun1(){
+    void fun1() {
         System.out.println("欢迎关注我的微信公众号——小鱼与Java");
+        String[] properties = {"a","b"};
+        List list = CollectionUtils.arrayToList(properties);
+        System.out.println(list.contains("b"));
     }
+
+
 }
